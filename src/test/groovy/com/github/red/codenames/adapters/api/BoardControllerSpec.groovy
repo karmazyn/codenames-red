b546243d3f3ea8bc2c04f7e8f8a584c97143f5a3
Malgorzata Karmazyn <karmazyn.m@gmail.com>
2021-01-29 16:38:44 +0100

    Added unit/int test for generating board

---
+++ src/test/groovy/com/github/red/codenames/adapters/api/BoardControllerSpec.groovy
@@ -0,0 +1,34 @@
+package com.github.red.codenames.adapters.api
+
+
+import groovyx.net.http.RESTClient
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.http.MediaType
+import org.springframework.test.context.ActiveProfiles
+import spock.lang.Specification
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles(["integration"])
+class BoardControllerSpec extends Specification {
+
+    @LocalServerPort
+    int port
+
+    def restClient
+
+    void setup() {
+        restClient = new RESTClient("http://localhost:$port", "application/json")
+        restClient.setHeaders(["Content-Type": MediaType.APPLICATION_JSON_VALUE, "Accept": MediaType.APPLICATION_JSON_VALUE])
+    }
+
+    def "should generate board"() {
+        given:
+        when:
+        def response = restClient.post(path: "/api/boards")
+
+        then:
+        response.status == 200
+        response.data.fields.size() == 25
+    }
+}
---
+++ src/test/groovy/com/github/red/codenames/domain/ports/BoardServiceSpec.groovy
@@ -0,0 +1,41 @@
+package com.github.red.codenames.domain.ports
+
+import com.github.red.codenames.domain.model.Board
+import com.github.red.codenames.domain.model.Card
+import com.github.red.codenames.domain.model.Team
+import com.github.red.codenames.domain.model.Type
+import com.github.red.codenames.domain.repository.BoardRepository
+import com.github.red.codenames.domain.repository.CardRepository
+import spock.lang.Specification
+
+class BoardServiceSpec extends Specification {
+
+    def cardRepository = Mock(CardRepository)
+    def boardRepository = Mock(BoardRepository)
+    def boardService = new BoardService(cardRepository, boardRepository)
+
+    def "should generate 5x5 board"() {
+        given:
+        def cards = (1..25).toList().collect { new Card("LCard#$it", "RCard$it") }
+        cardRepository.getCards(25) >> cards
+        boardRepository.save(_) >> {args -> args[0]}
+
+        when:
+        def board = boardService.generateBoard()
+
+        then:
+        board.fields.size() == 25
+        board.fields*.codename.unique().size() == 25
+        numberOfCards(board, Type.ASSASIN) == 1
+
+        assert ((numberOfCards(board, Type.RED) == 9 && numberOfCards(board, Type.BLUE) == 8)
+                || (numberOfCards(board, Type.RED) == 8 && numberOfCards(board, Type.BLUE) == 9))
+
+        assert ((board.starts == Team.RED && numberOfCards(board, Type.RED) == 9) ||
+                (board.starts == Team.BLUE && numberOfCards(board, Type.BLUE) == 9))
+    }
+
+    private static int numberOfCards(Board board, Type type) {
+        board.fields*.type.findAll { it == type }.size()
+    }
+}
