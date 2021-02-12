24953ac287a74a5ca22cd93094944aff542b5642
Mariusz Wojakowski <mariusz.wojakowski@allegro.pl>
2021-02-12 14:30:06 +0100

    Add react router

--- webapp/src/app/App.jsx
+++ webapp/src/app/App.jsx
@@ -1,92 +1,36 @@
 import React, {Component} from "react";
 import ReactDOM from 'react-dom';
 import '../css/App.css';
-import Board from "./Board";
-import Container from "@material-ui/core/Container";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import DashboardPanel from "./DashboardPanel";
-import Grid from "@material-ui/core/Grid";
-import TeamPanel from "./TeamPanel";
 
-import {connect, Provider} from "react-redux";
+import { Provider } from "react-redux";
 import store from "./redux/Store";
 
-import {initBoard, updateBoard} from "./redux/Actions";
-import {getBoardId} from "./redux/Selectors";
-import {CardCounter} from "./CardCounter";
 import { CookiesProvider } from 'react-cookie';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
+import { Lobby } from "./Lobby";
+import Game from "./Game";
 
 class App extends Component {
-    componentDidMount() {
-        this.createNewGame();
-    }
-
-    createNewGame() {
-        fetch("/api/game", {method: "POST", credentials: "same-origin"})
-            .then(response => response.json())
-            .then(gameInstance => {
-                fetch(`/api/game/${gameInstance.id}/start`, {method: 'POST'})
-                    .then(response => response.json())
-                    .then(gameInstance => {
-                        fetch(`/api/boards/${gameInstance.boardId}`)
-                            .then(response => response.json())
-                            .then(board => {
-                                this.props.initBoard({
-                                    boardId: board.id,
-                                    fields: board.fields,
-                                    starts: board.starts,
-                                    numberOfRed: board.numberOfRed,
-                                    numberOfBlue: board.numberOfBlue
-                                });
-                            })
-                            .then(() => this.updateBoardStateScheduleAsync());
-                    })
-            });
-    }
-
-    updateBoardStateScheduleAsync() {
-        let intervalId = setInterval(() => {
-            clearInterval(intervalId);
-            fetch(`/api/boards/${this.props.boardId}`)
-                .then(result => result.json())
-                .then(result => this.props.updateBoard(result))
-                .then(() => this.updateBoardStateScheduleAsync());
-        }, 1000);
-    }
-
     render() {
         return (
-            <React.Fragment>
-                <CssBaseline/>
-                <DashboardPanel/>
-                <Container id="main" className={"App"} maxWidth="large">
-                    <Grid container spacing={3}>
-                        <Grid item xs={2}>
-                            <TeamPanel/>
-                        </Grid>
-                        <Grid item xs={10}>
-                            <Board/>
-                        </Grid>
-                    </Grid>
-                </Container>
-                {/*<TeamChooser />*/}
-            </React.Fragment>
+            <main>
+                <Switch>
+                    <Route path="/" component={Game} exact />
+                    <Route path="/lobby" component={Lobby} />
+                    <Route component={Error} />
+                </Switch>
+            </main>
         );
     }
 }
 
-const mapStateToProps = state => {
-    return {
-        boardId: getBoardId(state),
-    };
-}
-
-const ConnectedApp = connect(mapStateToProps, { initBoard, updateBoard })(App);
 
 ReactDOM.render(
     <CookiesProvider>
         <Provider store={store}>
-            <ConnectedApp/>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
         </Provider>
     </CookiesProvider>,
     document.getElementById('root')
---
+++ webapp/src/app/Lobby.jsx
@@ -0,0 +1,11 @@
+import {Link} from "react-router-dom";
+import React from "react";
+
+export function Lobby() {
+    return (
+        <div>
+            <Link to="/">Take me hoooome...</Link>
+            <p>Welcome to the lobby!</p>
+        </div>
+    );
+}
